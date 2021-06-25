62eeec3b587735e51df5625b25e84ffe965ac58a
michael vargas roa <michael.vr25@hotmail.com>
2021-06-25 19:36:23 -0400

    sección 3 completada

--- PoolStudio.WEB/Controllers/UsersController.cs
+++ PoolStudio.WEB/Controllers/UsersController.cs
@@ -9,10 +9,23 @@ namespace PoolStudio.WEB.Controllers
     public class UsersController : Controller
     {
 
-        //[Route] = se puede utilizar para modificar una url, tanto para el controlador como para los métodos (index, edit, delete, etc).
+        // [Route] = se puede utilizar para modificar una url, tanto para el controlador como para los métodos (index, edit, delete, etc).
 
         public IActionResult Index(int id)
         {
+            /*
+               var url = Url.Action("Index", "Users", new
+            {
+                y = DateTime.Now.Year,
+                m = DateTime.Now.Month,
+                d = DateTime.Now.Day,
+                h = DateTime.Now.Hour,
+                ms = DateTime.Now.Minute,
+                s = DateTime.Now.Second,
+                mlls = DateTime.Now.Millisecond
+            });
+            */
+
             ViewData["id"] = id;
             return View();
         }
