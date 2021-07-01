b57e5e71936089d4ed19cad473f634d37d0ccf56
michael vargas roa <michael.vr25@hotmail.com>
2021-07-01 11:27:01 -0400

    seccion 8 completada

--- PoolStudio.WEB/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PoolStudio.WEB/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -45,19 +45,20 @@ namespace PoolStudio.WEB.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+            [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser al menos {2}.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Contraseña")]
             public string Password { get; set; }
 
+            [Required]
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
+            [Display(Name = "Confirmar Contraseña")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
--- PoolStudio.WEB/Controllers/UsersController.cs
+++ PoolStudio.WEB/Controllers/UsersController.cs
@@ -13,19 +13,6 @@ namespace PoolStudio.WEB.Controllers
 
         public IActionResult Index(int id)
         {
-            /*
-               var url = Url.Action("Index", "Users", new
-            {
-                y = DateTime.Now.Year,
-                m = DateTime.Now.Month,
-                d = DateTime.Now.Day,
-                h = DateTime.Now.Hour,
-                ms = DateTime.Now.Minute,
-                s = DateTime.Now.Second,
-                mlls = DateTime.Now.Millisecond
-            });
-            */
-
             ViewData["id"] = id;
             return View();
         }
