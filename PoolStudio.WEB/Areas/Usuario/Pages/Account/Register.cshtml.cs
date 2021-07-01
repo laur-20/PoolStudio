232e0ce03cb2d070013c516e5b0383b305193ca7
alejandro gil cardenas <alejandrogil98@hotmail.com>
2021-06-30 23:28:52 -0400

    Sección 7

--- PoolStudio.WEB/Areas/Usuario/Pages/Account/Register.cshtml.cs
+++ PoolStudio.WEB/Areas/Usuario/Pages/Account/Register.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,10 +10,48 @@ namespace PoolStudio.WEB.Areas.Usuario.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        public string Message { get; set; }
-        public void OnGet(string data)
+       
+        public void OnGet()
         {
-            Message = data;
+           
         }
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage ="Este campo es obligatorio.")]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage ="Este campo es obligatorio.")]
+            [DataType(DataType.Password)]
+            [Display(Name = "Contraseña")]
+            [StringLength(100, ErrorMessage = "El número de caracteres de {0} debe ser de al menos {2}.", MinimumLength = 6)]
+            public string Password { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "La confirmación de la contraseña no coincide con la contraseña.")]
+            public string ConfirmPassword { get; set; }
+
+
+            [Required]
+            public string ErrorMessage { get; set; }
+        }
+        public IActionResult OnPost() 
+        {
+            if(ModelState.IsValid)
+            {
+                 
+            }
+            else
+            {
+                ModelState.AddModelError("Input.Email", "Se ha generado un error en el servidor.");
+            }
+            var data = Input;
+            return Page();
+        } 
     }
 }
